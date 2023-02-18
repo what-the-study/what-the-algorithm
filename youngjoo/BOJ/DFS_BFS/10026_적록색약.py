7cacc644df6af0d294ea8ea216a8ed778732bf5f
kylekim2123 <kyle2123@naver.com>
2023-02-19 00:00:09 +0900

    [BOJ] 골드5 적록색약

---
+++ youngjoo/BOJ/DFS_BFS/10026_적록색약.py
@@ -0,0 +1,36 @@
+# 골드 5
+
+import sys
+
+sys.setrecursionlimit(10000)
+
+# 상 하 좌 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+normal = {'R': 3, 'B': 2, 'G': 1}
+weak = {'R': 2, 'B': 1, 'G': 2}
+
+
+def dfs(x, y, visited, status):
+    visited[x][y] = True
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n and status[grid[x][y]] == status[grid[nx][ny]] and not visited[nx][ny]:
+            dfs(nx, ny, visited, status)
+
+
+n = int(input())
+grid = [input() for _ in range(n)]
+visited1 = [[False] * n for _ in range(n)]
+visited2 = [[False] * n for _ in range(n)]
+count1, count2 = 0, 0
+for i in range(n):
+    for j in range(n):
+        if not visited1[i][j]:
+            dfs(i, j, visited1, normal)
+            count1 += 1
+        if not visited2[i][j]:
+            dfs(i, j, visited2, weak)
+            count2 += 1
+print(count1, count2)
