ddca2d3a0f29092b84b99e7b61d4392100217b88
kylekim2123 <kyle2123@naver.com>
2023-04-12 12:53:44 +0900

    [BOJ] 브론즈4 삼각형 외우기

---
+++ youngjoo/BOJ/Geometry/10101_삼각형_외우기.py
@@ -0,0 +1,13 @@
+# 브론즈 4
+
+a, b, c = int(input()), int(input()), int(input())
+
+if a == b == c == 60:
+    print("Equilateral")
+elif a + b + c == 180:
+    if a == b or b == c or a == c:
+        print("Isosceles")
+    else:
+        print("Scalene")
+else:
+    print("Error")
