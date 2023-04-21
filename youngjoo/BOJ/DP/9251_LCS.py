a137f0aadbe2f9dcbf5cd0bf508c5f7acfacf8cf
kylekim2123 <kyle2123@naver.com>
2023-04-22 00:13:34 +0900

    [BOJ] 골드5 LCS

---
+++ youngjoo/BOJ/DP/9251_LCS.py
@@ -0,0 +1,16 @@
+# 골드 5
+
+def lcs(s1, s2):
+    dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+
+    for i in range(1, len(s1) + 1):
+        for j in range(1, len(s2) + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+    return dp[-1][-1]
+
+
+print(lcs(input(), input()))
